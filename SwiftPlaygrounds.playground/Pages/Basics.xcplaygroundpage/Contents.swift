d0b75c871d8b842da295685611ecad97be056b98
ealpago <alpago@icloud.com>
2022-02-02 14:06:47 +0300

    Initial Commit

---
+++ SwiftPlaygrounds.playground/Pages/Basics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,63 @@
+//: [Previous](@previous)
+
+import Foundation
+import UIKit
+
+var greeting = "Hello, playground"
+
+//birth year never changes
+let birthYear = 2000
+//var can change like age
+var age = 21
+
+//var can change but not another type. Must be same type
+age = 22
+age = 23
+//or
+var tenYearsLaterAge = age + 10
+//cannot do
+// age = "22"
+// age = "twentyTwo"
+
+
+//declare multiple variables on a single line
+var deneme1 = "deneme1", deneme2 = "deneme2", deneme3 = "deneme3"
+
+
+//we can specify its type
+var myAge: Int
+let myBirthYear: Int
+let myName: String
+
+myAge = 21
+myBirthYear = 2000
+myName = "Emre"
+
+var usingInt: Int
+//cannot do
+// usingInt = "we cannot do that"
+usingInt = 111111
+
+var denemeString: String
+//cannot do
+// deneme string = 123456
+denemeString = "String"
+
+
+
+//This is a comments
+//We use this like taking note or reminder
+
+/* we can use multiple lines for comments
+ like this. */
+
+
+
+//Convert
+
+let pi = 3.14
+let integerPi = Int(pi)
+print(integerPi)
+
+
+
---
+++ SwiftPlaygrounds.playground/Pages/Closures.xcplaygroundpage/Contents.swift
@@ -0,0 +1,132 @@
+//: [Previous](@previous)
+
+import Foundation
+import UIKit
+import Darwin
+
+
+//Creating basic closures
+
+let walking = {
+    print("I'm walking now\n")
+}
+
+walking()
+
+print("----\n")
+
+//take parameter
+
+let walkTo = { (to: String) in
+    print("I'm walking to the \(to)\n")
+}
+
+walkTo("Galata")
+
+
+print("----\n")
+
+//return parameter
+
+let closureWithReturn = { (parameter: String) -> String in
+
+    return "This closure return this: \(parameter)\n"
+    
+}
+
+let returnClosure = closureWithReturn("deneme12")
+print(returnClosure)
+
+
+print("----\n")
+
+
+//you can return parameter without taking any parameter
+
+let payment = { () -> Bool in
+    
+    print("Customer paid\n")
+    return true
+}
+
+payment()
+
+
+print("----\n")
+
+
+//Closures as parameters
+//using walking closure on line 10
+
+func walk(action: () -> Void) {
+    action()
+    print("for cardio\n")
+}
+
+walk(action: walking)
+
+
+print("----\n")
+
+
+//Trailing closure syntax
+
+
+func trailing(exaampleClosure: () -> Void) {
+    print("first line ")
+    exaampleClosure()
+    print("third line")
+}
+
+trailing {
+    print("this is closure line")
+}
+
+
+print("----\n")
+
+
+//Closure as parameters -- accept parameters
+
+func printSomething(action: (String) -> Void) {
+    print("func print")
+    action("closure print")
+    print("Again func print\n")
+}
+
+printSomething { (action: String) in
+    print("This line \(action)")
+    
+}
+
+
+
+func study(reviseNotes: (String) -> Void) {
+    let notes = "Napoleon was a short, dead dude."
+    for _ in 1...10 {
+        reviseNotes(notes)
+    }
+}
+study { (notes: String) in
+    print("I'm reading my notes: \(notes)")
+}
+
+
+print("----\n")
+
+
+//Closure as parameters -- return values
+
+func returnValues(parameter: (String) -> String) {
+    print("Deneme123")
+    let takeValueOf = parameter("return closure string")
+    print(takeValueOf)
+}
+
+returnValues { (example: String) -> String in
+    return "This is \(example)\n"
+}
+
+print("----\n")
+
+
