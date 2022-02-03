48327a02db1f2b884ab3dc5232b14acac5e13582
ealpago <alpago@icloud.com>
2022-02-03 12:22:16 +0300

    classes

---
+++ SwiftPlaygrounds.playground/Pages/Classes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,89 @@
+import Foundation
+import UIKit
+
+
+//Basic class
+
+class Animal {
+    var name: String
+    var sound: String
+
+    init(name: String, sound: String) {
+        self.name = name
+        self.sound = sound
+    }
+}
+
+var dog = Animal(name:"Dog", sound:"wof")
+dog.name
+dog.sound
+
+//classes are reference type
+var differentDog = dog
+differentDog.name = "pitbull"
+print(differentDog.name)
+print(dog.name)
+
+print("\n----\n")
+
+//inheritance
+
+class Cow: Animal {
+    func giveMilk(){
+        print("Cows give milk ")
+    }
+}
+
+var cow = Cow(name:"Cow", sound:"Mö")
+cow.name
+cow.sound
+cow.giveMilk()
+
+print("\n----\n")
+
+//inheritance another example
+class Personal{
+    var department:String
+    var exp:Int
+    
+    init(department:String, exp:Int){
+        self.department = department
+        self.exp = exp
+    }
+    
+    func doWork(){
+        print("I'm currently working \(department) department")
+    }
+}
+
+class Manager: Personal{
+    var teamSize:Int = 0
+    
+    override func doWork() {
+        super.doWork()
+        print("And I manage \(teamSize) people")
+    }
+}
+
+var manager = Manager(department: "IT", exp: 10)
+manager.teamSize = 20
+manager.doWork()
+
+print("\n----\n")
+
+
+//Deinitializers
+
+class Singer{
+    var name:String
+    init(name:String){
+        self.name = name
+        print("\(name) is awesome person and made amazing song!")
+    }
+    deinit{
+        print("\(name) is no more with us")
+    }
+}
+
+var barisManco = Singer(name: "Barış Manço")
+//deiniti güncelle akşam toplantıda sor******
